0c4b05ff375bf995dec6f437bee5025f02b22a04
Yara M Esmaiel <yaraesmaiel123@gmail.com>
2022-12-30 10:42:17 -0500

    yyy

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
- 
-
 function App() {
   return (
-   <>
-     <div className="App">
-      
-       <h1>yara es</h1>
-     </div>
-   </>
+    <>
+      <div className="App">
+        <h1>modefied version 2</h1>
+        <h1>yara es</h1>
+      </div>
+    </>
   );
 }
 
